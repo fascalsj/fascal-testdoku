988a9b5a2d582b71245be33dd440c741e4ecdc78
Fascal Sapty Jarocohir <fascalsj@gmail.com>
2018-10-03 05:35:01 +0700

    penyelesaian kasus 2

--- case-2/src/main/java/com/case2/app/App.java
+++ case-2/src/main/java/com/case2/app/App.java
@@ -3,6 +3,7 @@ package com.case2.app;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -11,11 +12,16 @@ import java.util.HashMap;
 public class App {
 
     public static void main(String[] args) {
-        String kendaraan = "mobil";
+        System.out.print("Jenis Kendaraan : ");
+        String jenisKendaraan = new Scanner(System.in).next();
         HashMap<String, String[]> listParkiran = Fungsi.getParkiran();
-        System.err.println(listParkiran);
-        String [] mobil = listParkiran.get(kendaraan);
-        System.out.println(Arrays.toString(mobil));
+        String [] kendaraan = listParkiran.get(jenisKendaraan);
+        System.out.println("Lot Parkir yang diperbolehkan : ");
+        for (String kendaraan1 : kendaraan) {
+            System.out.println("   - Lot Parkir "+kendaraan1);
+        }
+        
+        
  
         
     }
--- case-2/src/main/java/com/case2/app/Fungsi.java
+++ case-2/src/main/java/com/case2/app/Fungsi.java
@@ -12,32 +12,13 @@ public class Fungsi {
 
     public static HashMap<String, String[]> getParkiran() {
         
-        String [] motor = {"motor,mobil,bus"};
-        String [] mobil = {"mobil,bus"};
+        String [] motor = {"motor","mobil","bus"};
+        String [] mobil = {"mobil","bus"};
         String [] bus = {"bus"};
         HashMap<String, String[]> parkiran = new HashMap<String, String[]>();
         parkiran.put("motor",motor);
         parkiran.put("mobil",mobil);
         parkiran.put("bus",bus);
         return parkiran;
-
-        
-//        ArrayList<String> motor = new ArrayList<String>();
-//        motor.add("motor");
-//        motor.add("mobil");
-//        motor.add("bus");
-//        
-//        ArrayList<String> mobil = new ArrayList<String>();
-//        mobil.add("mobil");
-//        mobil.add("bus");
-//
-//        ArrayList<String> bus = new ArrayList<String>();
-//        bus.add("bus");
-//
-//        ArrayList<ArrayList<String>> parkiran = new ArrayList();
-//        parkiran.add(mobil);
-//        parkiran.add(motor);
-//        parkiran.add(bus);
-//        return parkiran;
     }
 }
